7aff54dee9392e27e5e3ee0e9dd5a02ef6ead16f
Bryan <127118299+BryanAbreu21@users.noreply.github.com>
2023-08-31 18:43:04 -0300

    Implementação sub main
    Implementação sub main

--- scr/main/java/TDD-2.Main/Calculadora.java
+++ scr/main/java/TDD-2.Main/Calculadora.java
@@ -6,6 +6,9 @@ public class Calculadora{
         return a * b;
     }
     public int divi(int a, int b){
-        return a/b; 
+        return a / b; 
+    }
+    public int sub(int a, int b){
+        return a - b;
     }
 }
